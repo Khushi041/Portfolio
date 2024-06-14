e34521d562ef312e3b3d5e29087c34896ad502b4
khushi_parmar <Khushiparmar0410@gmail.com>
2024-06-14 14:02:02 +0530

    resume update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
         </>
       ),
       // href: 'portfoliopublicResume.pdf',
-      href: '/Resume.pdf',
+      href: '/Khushi.pdf',
       style: 'rounded-br-md',
       download: true,
     },
